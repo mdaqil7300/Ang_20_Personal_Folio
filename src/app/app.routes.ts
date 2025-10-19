4dde7101f371b154ebbb3f8cf5b5e519f69fa68b
mdaqil7300 <mdaqil7300@gmail.com>
2025-10-19 18:28:43 +0530

    changed resume buton font

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,3 @@
-// import { Routes } from "@angular/router";
-// import { HeroComponent } from "./components/hero/hero.component";
-// import { AboutComponent } from "./components/about/about.component";
-// import { SkillsComponent } from "./components/skills/skills.component";
-// import { ExperienceComponent } from "./components/experience/experience.component";
-// import { EducationComponent } from "./components/education/education.component";
-// import { ContactComponent } from "./components/contact/contact.component";
+import { Routes } from "@angular/router";
 
-// export const appRoutes: Routes = [
-//   { path: "", component: HeroComponent },
-//   { path: "about", component: AboutComponent },
-//   { path: "skills", component: SkillsComponent },
-//   { path: "experience", component: ExperienceComponent },
-//   { path: "education", component: EducationComponent },
-//   { path: "contact", component: ContactComponent },
-// ];
+export const appRoutes: Routes = [];
